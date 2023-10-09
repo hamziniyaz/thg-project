a7dfe7d347c7291edf7f5bdceec4c174fc2885b9
hamzi <hamziniyaz@gmail.com>
2023-10-09 14:30:28 +0800

    fix route

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ProductController;
 
 // Main Page Route
-Route::get('/', $controller_path . [ProductController::class, 'index']);
+Route::get('/', [ProductController::class, 'index']);
 Route::resource('/products', ProductController::class);
 Route::delete('/products/image/delete', [ProductController::class, 'imageDeletion'])->name('products.image.delete');
 Route::get('/detail/products/{product}', [ProductController::class, 'show'])->name('prod.detail');
